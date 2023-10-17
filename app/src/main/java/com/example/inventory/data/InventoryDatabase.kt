9ce1ce4c80330afa98334f4fb0564ed442f1d08b
DudaArinvs <eduarda.arvins@ua.pt>
2023-10-17 16:00:49 +0100

    Inventory App from the codelabs, testing for now incomplete

---
+++ app/src/main/java/com/example/inventory/data/InventoryDatabase.kt
@@ -0,0 +1,26 @@
+package com.example.inventory.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+
+@Database(entities = [Item::class], version = 1, exportSchema = false)
+//Item as the only class with a list of entities, schema changes, not keep schema version history
+abstract class InventoryDatabase : RoomDatabase() {
+    abstract fun itemDao(): ItemDao //let the database know about the DAO
+    companion object{
+        @Volatile
+        private var Instance: InventoryDatabase? = null
+
+        fun getDatabase(context: Context): InventoryDatabase {
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, InventoryDatabase::class.java, "item_database")
+                    .fallbackToDestructiveMigration()
+                    .build()
+                    .also { Instance = it }
+            }
+        }
+    }
+}
